3e2148d2ba1f75d4e4cc82e7545618fee4835a29
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-10 12:19:22 +0100

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Integer> oldList = new LinkedList<>();
+        oldList.add(1);oldList.add(1);oldList.add(2);oldList.add(0);oldList.add(4);oldList.add(5);oldList.add(6);oldList.add(8);oldList.add(8);
+        oldList.add(3);oldList.add(11);oldList.add(9);oldList.add(0);oldList.add(14);oldList.add(0);oldList.add(16);
+        removeEvenInRage(oldList,5,13);
+
 
     }
-    public static LinkedList<Integer> removeEvenInRage(LinkedList<Integer> list, int start, int end){
 
-    
+    public static void removeEvenInRage(LinkedList<Integer> oldList, int start, int end) {
+        int index = start;
+        List<Integer> tempList = oldList.subList(start, end);
+        Iterator<Integer> itr = tempList.iterator();
 
+        while (itr.hasNext()) {
+            Integer odd = itr.next();
+            if (odd % 2 == 0) {
+                itr.remove();
+            }
+        }
+        System.out.println(oldList);
     }
 }
+
+
+
+
+/*
+            for (Iterator<Integer> it = oldList.iterator(); it.hasNext(); ) {
+                Integer odd = it.next();
+                if (odd % 2 == 0) {
+                    it.remove();
+                    index++;
+                }
+            }
+*/
+
