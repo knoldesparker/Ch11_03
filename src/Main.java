1f3cadc878ca36e24f0788d20556a2f49ec60f23
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-10 10:51:05 +0100

    setting it up

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+    }
+    public static LinkedList<Integer> removeEvenInRage(LinkedList<Integer> list, int start, int end){
+
+    
+
+    }
+}
