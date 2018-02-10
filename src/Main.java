61411afaf93c4097cd2a2b6e1127781096726e20
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-10 12:24:58 +0100

    commentes

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Makes the list that needs to be sortet in
         LinkedList<Integer> oldList = new LinkedList<>();
+        //adds alot of values
         oldList.add(1);oldList.add(1);oldList.add(2);oldList.add(0);oldList.add(4);oldList.add(5);oldList.add(6);oldList.add(8);oldList.add(8);
         oldList.add(3);oldList.add(11);oldList.add(9);oldList.add(0);oldList.add(14);oldList.add(0);oldList.add(16);
+        //calls the method with the parameters to the list and start and end values
         removeEvenInRage(oldList,5,13);
-
-
     }
 
     public static void removeEvenInRage(LinkedList<Integer> oldList, int start, int end) {
-        int index = start;
+        //Makes a tempList of the old list in the selected index
         List<Integer> tempList = oldList.subList(start, end);
+        //Sets the iterator to be the templist
         Iterator<Integer> itr = tempList.iterator();
 
+        //runs while the list still have more elements and removeds the odd ones from the list
+        //that have been placed in the tempList
         while (itr.hasNext()) {
             Integer odd = itr.next();
             if (odd % 2 == 0) {
